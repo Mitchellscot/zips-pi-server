9599bdd4dfebf1630d2a9e6637f3043cd30bbd94
mitchellscot <Mitchellscott@me.com>
2021-02-23 10:24:56 -0600

    seems to be working, need to test on the pi

--- server/modules/imageWatcher.js
+++ server/modules/imageWatcher.js
@@ -1,38 +1,38 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
-
+require('dotenv').config();
 
 function imageWatcher() {
     const s3 = new AWS.S3({
-        accessKeyId: ACCESS_KEY_ID,
-        secretAccessKey: SECRET_ACCESS_KEY
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
     }
-
     );
-    const path = PATH;
+    const path = "/home/mitch/Pictures/motion";
     fs.watch(path, (eventType, filename) => {
         const nameAndPath = path + "/" + filename
         if (fs.existsSync(nameAndPath)) {
             const fileContent = fs.readFileSync(nameAndPath);
             const params = {
-                Bucket: BUCKET_NAME,
+                Bucket: process.env.BUCKET_NAME,
                 Key: filename,
                 Body: fileContent,
                 ContentType: "image/jpeg"
             }
-            s3.upload(params, (error, data) => {
-                if (error) {
-                    throw error;
-                }
-                console.log(`Image store in this url ${data.Location}`);
-            }).then(
+            var putObjectPromise = s3.upload(params).promise();
+            putObjectPromise.then(function(data) {
+                console.log('Success', data.Location);
                 fs.unlink(`/home/mitch/Pictures/motion/${filename}`, (err) => {
                     if (err) throw err;
                 })
-            )
-
+              }).catch(function(err) {
+                console.log(err);
+              });    
         }
     });
 }
+
+
+
 
 module.exports = imageWatcher;
